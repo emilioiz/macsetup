36bcb39efeb3cb21fa48089c43c3d483bd37a6df
Emilio R Izquierdo <55839898+emilioiz@users.noreply.github.com>
2022-05-11 13:23:13 -0400

    Update macsetup.sh
    change nvm to use brew

--- macsetup.sh
+++ macsetup.sh
@@ -29,7 +29,11 @@ brew install --cask postman
 
 # nvm
  
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | zsh
+brew install nvm
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
  
 nvm install 14
 
